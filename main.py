3b0ff955469c0b04a4a2595452acd042b744b0ff
dxu13 <126931493+dxu13@users.noreply.github.com>
2023-03-04 20:55:59 +0800

    feature/test1 (#2)
    * feature/test1 init
    
    * feature_test1 complete

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 if __name__ == '__main__':
     print("main function added & development complete")
     print("feature test2 development complete")
-    
-
+    print("feature test1 development completed")
