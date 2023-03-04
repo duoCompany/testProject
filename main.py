4c3e180bd5c8ba023c2c3e6a3fad06a5cee36bdd
Duo Xu <duo.xu@nike.com>
2023-03-04 21:04:40 +0800

    release issue fix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 if __name__ == '__main__':
     print("main function added & development complete")
     print("feature test2 development complete")
-    print("feature test1 development completed")
+    print("feature test1 development completed & issue fix")
